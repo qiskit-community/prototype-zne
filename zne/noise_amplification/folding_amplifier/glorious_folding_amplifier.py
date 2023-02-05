78dc66268c9cdf86d070029635bf4d714f782d85
Vishal Bajpe <Vishal.Bajpe@ibm.com>
2023-02-06 00:42:32 +0800

    docs(folding_amplifier): make docs a bit nicer

--- zne/noise_amplification/folding_amplifier/glorious_folding_amplifier.py
+++ zne/noise_amplification/folding_amplifier/glorious_folding_amplifier.py
@@ -24,9 +24,15 @@ from ..noise_amplifier import DAGNoiseAmplifier
 class GloriousFoldingAmplifier(DAGNoiseAmplifier):
     """Alternatingly composes the circuit and its inverse as many times as indicated
     by the ``noise_factor``.
+
+    References:
+        [1] T. Giurgica-Tiron et al. (2020).
+            Digital zero noise extrapolation for quantum error mitigation.
+            `<https://ieeexplore.ieee.org/document/9259940>`
     """
 
     def amplify_dag_noise(self, dag: DAGCircuit, noise_factor: Any) -> DAGCircuit:
+        """Applies global folding to input DAGCircuit and returns amplified circuit"""
         noise_factor = self._validate_noise_factor(noise_factor)
         num_full_foldings = self._compute_folding_nums(noise_factor)
         return self._apply_full_folding(dag, num_full_foldings)
@@ -36,7 +42,15 @@ class GloriousFoldingAmplifier(DAGNoiseAmplifier):
         dag: DAGCircuit,
         num_foldings: int,
     ) -> DAGCircuit:
-        """Fully folds the original DAG circuit a number of ``num_foldings`` times."""
+        """Fully folds the original DAG circuit a number of ``num_foldings`` times.
+
+        Args:
+            dag (DAGCircuit): The original dag circuit without foldings.
+            num_foldings (float): Number of times the circuit should be folded.
+
+        Returns:
+            DAGCircuit: The noise amplified DAG circuit.
+        """
         barrier = Barrier(dag.num_qubits())
         inverse_dag = self._invert_dag(dag)
         noisy_dag = copy.deepcopy(dag)
@@ -49,7 +63,14 @@ class GloriousFoldingAmplifier(DAGNoiseAmplifier):
         return noisy_dag
 
     def _invert_dag(self, dag_to_inverse: DAGCircuit) -> DAGCircuit:
-        """Inverts a dag circuit on a copy of the original dag"""
+        """Inverts a dag circuit on a copy of the original dag
+
+        Args:
+            dag_to_inverse (DAGCircuit) : The original dag circuit to invert.
+
+        Returns:
+            DAGCircuit: The inverted DAG circuit.
+        """
         inverted_dag = dag_to_inverse.copy_empty_like()
         for node in dag_to_inverse.topological_op_nodes():
             inverted_dag.apply_operation_front(
@@ -58,7 +79,16 @@ class GloriousFoldingAmplifier(DAGNoiseAmplifier):
         return inverted_dag
 
     def _validate_noise_factor(self, noise_factor: Any) -> float:
-        """Normalizes and validates noise factor."""
+        """Normalizes and validates noise factor.
+        Args:
+            noise_factor (float) : The original noisefactor input.
+
+        Returns:
+            float: Normalised noisefactor input
+
+        Raises:
+            ValueError: If input noise_factor value is not of type float
+        """
         try:
             noise_factor = float(noise_factor)
         except (ValueError, TypeError):
@@ -79,5 +109,11 @@ class GloriousFoldingAmplifier(DAGNoiseAmplifier):
         return noise_factor
 
     def _compute_folding_nums(self, noise_factor: float) -> int:
-        """Compute num foldings."""
+        """Compute num foldings.
+        Args:
+            noise_factor (float) : The original noise_factor input.
+
+        Returns:
+            int: Number of foldings calculated from noise_factor
+        """
         return int((noise_factor - 1) / 2)
