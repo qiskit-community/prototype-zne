c66f2aaf1f4026aed433f22d8b89a2b031a42c73
Vishal Bajpe <Vishal.Bajpe@ibm.com>
2023-02-14 21:56:38 +0800

    refactor(folding_amplifier): improve validate_gates_to_fold

--- zne/noise_amplification/folding_amplifier/glorious_local_folding_amplifier.py
+++ zne/noise_amplification/folding_amplifier/glorious_local_folding_amplifier.py
@@ -33,18 +33,16 @@ class GloriousLocalFoldingAmplifier(DAGNoiseAmplifier):
             `<https://ieeexplore.ieee.org/document/9259940>`
     """
 
-    def amplify_dag_noise(  # type: ignore # pylint: disable=arguments-differ
-        self,
-        dag: DAGCircuit,
-        noise_factor: float,
-        gates_to_fold: Sequence[int | str] | None,
+    def __init__(self, gates_to_fold: Sequence[int | str], barriers: bool = True) -> None:
+        self.gates_to_fold = self._validate_gates_to_fold(gates_to_fold)
+        self.barriers = barriers
+
+    def amplify_dag_noise(  # pylint: disable=arguments-differ
+        self, dag: DAGCircuit, noise_factor: float
     ) -> DAGCircuit:
         """Applies local folding to input DAGCircuit and returns amplified circuit"""
         num_full_foldings = self._compute_num_foldings(noise_factor)
-        return self._apply_local_folding(dag, num_full_foldings, gates_to_fold)
-
-        # TODO: Find graceful way of implementing this without overriding typing
-        # TODO: Find way to work with standalone int and str instead of lists
+        return self._apply_local_folding(dag, num_full_foldings, self.gates_to_fold)
 
     def _apply_local_folding(
         self,
@@ -55,25 +53,24 @@ class GloriousLocalFoldingAmplifier(DAGNoiseAmplifier):
         """Applies local folding strategy and returns a noise amplified dag.
 
         Args:
-            dag (DAGCircuit): The original dag circuit without foldings.
-            num_foldings (float): Number of times the circuit should be folded.
-            gates_to_fold (Sequence[int | str] | None) : Original gates_to_fold input.
+            dag: The original dag circuit without foldings.
+            num_foldings: Number of times the circuit should be folded.
+            gates_to_fold: Original gates_to_fold input.
 
         Returns:
             DAGCircuit: The noise amplified DAG circuit.
         """
-        self._validate_gates_to_fold(gates_to_fold)
         if gates_to_fold in [0, "0", None]:
             return dag
         noisy_dag = dag.copy_empty_like()
         for node in dag.topological_op_nodes():
             if node.name in gates_to_fold or node.op.num_qubits in gates_to_fold:
-                noisy_dag = self._insert_folding(noisy_dag, node, num_foldings)
+                noisy_dag = self._apply_folded_operation_back(noisy_dag, node, num_foldings)
             else:
                 noisy_dag.apply_operation_back(node.op, qargs=node.qargs, cargs=node.cargs)
         return noisy_dag
 
-    def _insert_folding(
+    def _apply_folded_operation_back(
         self,
         dag: DAGCircuit,
         node: DAGOpNode,
@@ -82,21 +79,24 @@ class GloriousLocalFoldingAmplifier(DAGNoiseAmplifier):
         """Folds each gate of original DAG circuit a number of ``num_foldings`` times.
 
         Args:
-            dag (DAGCircuit): The original dag circuit without foldings.
-            num_foldings (float): Number of times the circuit should be folded.
+            dag: The original dag circuit without foldings.
+            num_foldings: Number of times the circuit should be folded.
 
         Returns:
             DAGCircuit: The noise amplified DAG circuit.
         """
-        barrier = Barrier(node.op.num_qubits)
+        if self.barriers:
+            barrier = Barrier(node.op.num_qubits)
         dag.apply_operation_back(barrier, qargs=node.qargs)
         dag.apply_operation_back(node.op, qargs=node.qargs, cargs=node.cargs)
         inverted_node = node.op.inverse()
         for _ in range(num_foldings):
-            dag.apply_operation_back(barrier, qargs=node.qargs)
+            if self.barriers:
+                dag.apply_operation_back(barrier, qargs=node.qargs)
             dag.apply_operation_back(inverted_node, qargs=node.qargs, cargs=node.cargs)
             dag.apply_operation_back(barrier, qargs=node.qargs)
-            dag.apply_operation_back(node.op, qargs=node.qargs, cargs=node.cargs)
+            if self.barriers:
+                dag.apply_operation_back(node.op, qargs=node.qargs, cargs=node.cargs)
         dag.apply_operation_back(barrier, qargs=node.qargs)
         return dag
 
@@ -104,7 +104,7 @@ class GloriousLocalFoldingAmplifier(DAGNoiseAmplifier):
         """Compute number of foldings.
 
         Args:
-            noise_factor (float) : The original noise_factor input.
+            noise_factor : The original noise_factor input.
 
         Returns:
             int: Number of foldings calculated from noise_factor.
@@ -112,23 +112,29 @@ class GloriousLocalFoldingAmplifier(DAGNoiseAmplifier):
         noise_factor = self._validate_noise_factor(noise_factor)
         return int((noise_factor - 1) / 2)
 
-    def _validate_gates_to_fold(self, gates_to_fold: Sequence[int | str] | None) -> None:
+    def _validate_gates_to_fold(
+        self, gates_to_fold: Sequence[int | str] | int | str | None
+    ) -> tuple[int | str, ...]:
         """Validates if gates_to_fold is valid.
 
         Args:
             gates_to_fold (Sequence[int | str] | None) : Original gates_to_fold input.
         """
+        if gates_to_fold is None:
+            return ()
         if isinstance(gates_to_fold, int):
-            return
-        gate_list = list(standard_gates.get_standard_gate_name_mapping().keys())
+            return (gates_to_fold,)
+        if isinstance(gates_to_fold, str):
+            gates_to_fold = (gates_to_fold,)
+        VALID_GATES = set(  # pylint: disable=invalid-name
+            standard_gates.get_standard_gate_name_mapping().keys()
+        )
         for value in gates_to_fold:
-            if value not in gate_list:
-                try:
-                    int(value)
-                except ValueError:
-                    raise ValueError(  # pylint: disable=raise-missing-from
-                        f"{value} not a valid gate or value."
-                    )
+            bad_int = isinstance(value, int) and value <= 0
+            bad_str = isinstance(value, str) and value not in VALID_GATES
+            if bad_int or bad_str:
+                raise ValueError(f"{value!r} not a valid gate to fold option.")
+        return tuple(gates_to_fold)
 
     # pylint: disable=duplicate-code
 
@@ -136,7 +142,7 @@ class GloriousLocalFoldingAmplifier(DAGNoiseAmplifier):
         """Normalizes and validates noise factor.
 
         Args:
-            noise_factor (float) : The original noisefactor input.
+            noise_factor : The original noisefactor input.
 
         Returns:
             float: Normalised noisefactor input.
