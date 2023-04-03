c8cd7c9a2993c1203f3e2ca4ae02ca1e2c5b5252
Guillermo Mijares Vilarino <Guillermo.Mijares.Vilarino@ibm.com>
2023-04-03 10:19:23 +0200

    change html theme to qiskit_sphinx_theme

--- docs/conf.py
+++ docs/conf.py
@@ -24,5 +24,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'qiskit_sphinx_theme'
 html_static_path = ['_static']
